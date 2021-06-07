f17424ac9f3ec89b6b92afaf45d6e748a075fa11
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-07 16:35:27 +0200

    the end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,31 +64,15 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+       <div class="content">
+            <div class="title m-b-md">
+                Ciao
+            </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Ciao
-                </div>
-
-                <div class="links">
-                    <a href="{{ route('home')}}">Homepage</a>
-                    <a href="{{ route('about')}}">About</a>
-                    <a href="{{ route('news')}}">News</a>
-                </div>
+            <div class="links">
+                <a href="{{ route('home')}}">Homepage</a>
+                <a href="{{ route('about')}}">About</a>
+                <a href="{{ route('news')}}">News</a>
             </div>
         </div>
     </body>
